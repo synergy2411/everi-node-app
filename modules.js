7e46710dbca1bbe8a3a96a98a6e69230f21d4bfc
synergy2411 <synergy2411@outlook.com>
2019-07-19 11:33:41 +0530

    first commit

---
+++ modules.js
@@ -0,0 +1,54 @@
+// External Modules
+//  - npmjs.com
+
+// npm install chalk --save
+// npm init
+
+// let chalk = require("chalk");
+// console.log(chalk.green("Welcome to Node"));
+// console.log(chalk.inverse.green("Welcome to Node"));
+// console.log(chalk.bold.red("Welcome to Node"));
+
+
+// Native / Core Module
+// const path = require("path");
+
+// const url = "http://www.example.com/public/index.html";
+
+// console.log(path.extname(url));
+// console.log(path.basename(url));
+// console.log(path.dirname(url));
+
+// const os = require("os");
+// console.log("Total Memory : ", os.totalmem());
+// console.log("Arch :  : ", os.arch());
+// console.log("CPU'S :  : ", os.cpus().length);
+
+
+// const http = require("http");
+
+// const server = http.createServer((request, response)=>{
+//     console.log("Method : ", request.method);
+//     // console.log(request);
+
+//     response.write("Hello from Server");
+
+//     response.end();
+// })
+
+// server.listen(9090, ()=>{
+//     console.log("Server started at 9090...");
+// })
+
+
+const fs = require("fs");
+
+// fs.readFile("./package.json", (err, data)=>{
+//     console.log(data.toString());
+// })
+
+const data = fs.readFileSync("./package.json");
+
+console.log(data);
+
+console.log("Hello FS");
